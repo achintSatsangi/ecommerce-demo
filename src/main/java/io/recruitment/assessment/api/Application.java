74e1052c2b5313de60f7790ce367b7095fac5ff7
achint.satsangi <achint.satsangi@cgi.com>
2021-02-07 11:48:14 +0100

    Some files missed in previous commit

--- src/main/java/io/recruitment/assessment/api/Application.java
+++ src/main/java/io/recruitment/assessment/api/Application.java
@@ -1,7 +1,10 @@
 package io.recruitment.assessment.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,8 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
